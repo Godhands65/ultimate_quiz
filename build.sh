74bfed58aef425c38f78d973c440e580b2dc1cf1
Kader <Ka6570der@gmail.com>
2025-05-12 14:57:20 +0000

    La total

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "adminpass123")
+    User.objects.create_superuser("Kader", "Ka6570der@gmail.com", "Ka65fan70")
 EOF
